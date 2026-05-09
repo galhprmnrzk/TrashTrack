c3435a7e7d542c2cd19938f2d645a939227a1fd0
rasyairham <rasyairham@gmail.com>
2026-05-09 16:14:11 +0700

    Switch root directory to monorepo

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path' // Tambahkan ini
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/', 
   resolve: {
     alias: {
-      // Ini akan membuat '@' merujuk ke folder 'src'
+      // Menggunakan path.join dan __dirname agar aman di Linux/Vercel
       '@': path.resolve(__dirname, './src'),
     },
   },
+  build: {
+    outDir: 'dist',
+  }
 })
